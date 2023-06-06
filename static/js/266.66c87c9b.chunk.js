0a6b91bd5e4476c0d29e25aa26ed74fccedcf423
tSolodkaya <77572918+tSolodkaya@users.noreply.github.com>
2023-06-06 17:49:58 +0000

    Deploying to gh-pages from @ tSolodkaya/tweets@4fa2cbc96ab7ed1382efac31d1ee32d873841a48 🚀

--- static/js/266.66c87c9b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)("h1",{children:"Welcome to tweets"})})}}}]);
-//# sourceMappingURL=266.66c87c9b.chunk.js.map
